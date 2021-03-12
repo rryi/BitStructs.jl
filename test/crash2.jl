f16c15dd21359c289a319ea936d08177769e7e9c
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-12 11:27:36 +0100

    drilldown

--- test/crash2.jl
+++ test/crash2.jl
@@ -56,58 +56,9 @@ for memory efficiency (Strange,Sign), demonstrating how a custom encoding can ov
     delta2 :: BInt{9} # -256..255 
 end
 
-"""
-This struct has fields with the same names as BS and field types
-compatible to the types in BS with the smallest size available in
-standard julia types to cover the encoded value range. 
-
-It is declared mutable for two reasons: 
-    (a) it is quite large - 28 bytes
-    (b) it contains several fields which are due to frequent updates
-
-
-
-"""
-mutable struct S 
-    status :: ProcStatus # could be the overall status of some process
-    strange :: Strange
-    sign :: Float16 # typeof(1.0)===Float64, but that would be overkill ;-) 
-    flag1 :: Bool
-    flag2 :: Bool
-    flag3 :: Bool # to be honest: many flags push memory savings and runtime advantages of this BitStruct
-    flag4 :: Bool # you could always use Bool for a one-bit field, but ...
-    bit1  :: Int8 # in a numerical context, BInt/BUInt does the number conversion for you
-    ac :: Char # if you know a Char is ASCII, encode it in 7 instead of 32 bits
-    lc :: Char # similar use 8 bits for the Latin-1 character set
-    id1 :: UInt16 # 0..511
-    id2 :: UInt16 # 0..4095
-    delta1 :: Int16 # -256..255
-    delta2 :: Int16 # -256..255 
-end
-#Base.copy(x::T) where T<:S = T([getfield(x, k) for k ∈ fieldnames(T)]...)
-
-
-s = S(S_RUNNING,BIGMINUS,Float16(-1.0),true,false,false,true,0%Int8,'a','c',0x0001,0x0002,3%Int16,4%Int16)
 bs = BS(S_RUNNING,BIGMINUS,-1.0,true,false,false,true,0,'a','c',0x1,0x2,3,4)
 
-show(s)
-
 show(bs)
-
-
-
-
-
-## some simple field access benchmarks to begin with
-
-mutable struct T
-    id1::UInt16
-    id2::UInt16
-    flag1::Bool
-    flag2::Bool
-end
-Base.copy(x::T) = T([getfield(x, k) for k ∈ fieldnames(T)]...)
-
 
 @noinline function set2fields(s)
     if typeof(s) <: BitStruct
@@ -120,7 +71,7 @@ Base.copy(x::T) = T([getfield(x, k) for k ∈ fieldnames(T)]...)
 end
 
 println("the statements executed by set2fields run, if executed directly")
-bs /= :id1, s.id2
+bs /= :id1, bs.id2
 bs /= :flag1,bs.flag2
 show(bs)
 println("Calling set2fields(bs) causes a crash")
