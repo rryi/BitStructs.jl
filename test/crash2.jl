b624aa0685c7f1ca07ba97251a919a255c469cb3
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-12 12:06:44 +0100

    minimal crash with set2fields4, weird error with set2fields7

--- test/crash2.jl
+++ test/crash2.jl
@@ -54,6 +54,12 @@ function set2fields6(bs::T) where T <: BitStruct
 end
 
 
+function set2fields7(bs::T) where T <: BitStruct
+    bs = BitStruct.set(bs, :id1, bs.id2)
+    bs /= :flag1, bs.flag2
+end
+
+
 #println("the statements executed by set2fields run, if executed directly")
 #bs /= :id1, bs.id2 # no crash
 #bs /= :flag1,bs.flag2 # no crash
@@ -65,4 +71,16 @@ println("Calling set2fields(bs) causes a crash")
 #set2fields4(bs) # crash
 set2fields5(bs) # NO crash !!
 set2fields6(bs) # NO crash !!
-#set2fields4(bs) # crash
+set2fields7(bs) # NO crash but weird error message:
+#=
+ERROR: LoadError: type UnionAll has no field set
+Stacktrace:
+ [1] getproperty(x::Type, f::Symbol)
+   @ Base .\Base.jl:28
+ [2] set2fields7(bs::BitStruct{NamedTuple{(:flag1, :flag2, :id1, :id2), Tuple{Bool, Bool, BUInt{9}, BUInt{12}}}})
+   @ Main ~\julia\BitStructs\test\crash2.jl:58
+ [3] top-level scope
+   @ ~\julia\BitStructs\test\crash2.jl:74
+in expression starting at c:\Users\RR\julia\BitStructs\test\crash2.jl:74
+=#
+
