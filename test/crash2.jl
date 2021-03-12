f5ae3c08a5cb7b36d1918788a5193ed921c6a025
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-12 11:54:26 +0100

    no crash when simplifying set2fields4

--- test/crash2.jl
+++ test/crash2.jl
@@ -40,11 +40,29 @@ function set2fields3(bs::T) where T <: BitStruct
     end
 end
 
+function set2fields4(bs::T) where T <: BitStruct
+        bs /= :id1, bs.id2
+        bs /= :flag1, bs.flag2
+end
+
+function set2fields5(bs::T) where T <: BitStruct
+    bs /= :id1, bs.id2
+end
+
+function set2fields6(bs::T) where T <: BitStruct
+    bs /= :flag1, bs.flag2
+end
+
+
 #println("the statements executed by set2fields run, if executed directly")
-#bs /= :id1, bs.id2
-#bs /= :flag1,bs.flag2
+#bs /= :id1, bs.id2 # no crash
+#bs /= :flag1,bs.flag2 # no crash
 #show(bs)
 println("Calling set2fields(bs) causes a crash")
-#set2fields(bs)
-#set2fields2(bs) 
-set2fields3(bs) 
+#set2fields(bs) # crash
+#set2fields2(bs) # crash
+#set2fields3(bs) # crash
+#set2fields4(bs) # crash
+set2fields5(bs) # NO crash !!
+set2fields6(bs) # NO crash !!
+#set2fields4(bs) # crash
