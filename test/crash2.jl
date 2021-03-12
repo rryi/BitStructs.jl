130c8a9d3ab62d47bb477732546569cee123d35f
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-12 11:31:53 +0100

    crash

--- test/crash2.jl
+++ test/crash2.jl
@@ -48,15 +48,15 @@ for memory efficiency (Strange,Sign), demonstrating how a custom encoding can ov
     flag3 :: Bool # to be honest: many flags push memory savings and runtime advantages of this BitStruct
     flag4 :: Bool # you could always use Bool for a one-bit field, but ...
     bit1  :: BInt{1} # in a numerical context, BInt/BUInt does the number conversion for you bit1 can be -1, 0
-    ac :: AsciiChar # if you know a Char is ASCII, encode it in 7 instead of 32 bits
-    lc :: Latin1Char # similar use 8 bits for the Latin-1 character set
+    #ac :: AsciiChar # if you know a Char is ASCII, encode it in 7 instead of 32 bits
+    #lc :: Latin1Char # similar use 8 bits for the Latin-1 character set
     id1 :: BUInt{9} # 0..511
     id2 :: BUInt{12} # 0..4095
     delta1 :: BInt{9} # -256..255
     delta2 :: BInt{9} # -256..255 
 end
 
-bs = BS(true,false,false,true,0,'a','c',0x1,0x2,3,4)
+bs = BS(true,false,false,true,0,0x1,0x2,3,4)
 
 show(bs)
 
