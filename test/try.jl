17785b54ae3580b59df97b4aba7f21897b815a45
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-18 11:22:13 +0100

    d2

--- test/try.jl
+++ test/try.jl
@@ -1,51 +1,62 @@
 using BitStructs
 
-
+# abstract example
 module M
 
 function bitsizeof(::Type{T}) where T<: Enum 
-    # use "new world" in all function calls
+    # use "new world" in all function calls depending on T
     8*sizeof(Int) - leading_zeros(Int(Base.invokelatest(typemax,T))-Int(Base.invokelatest(typemin,T)))
 end
 
-#=
-@generated function f(::Type{T},::Val{s}) where {T<:Enum , s}
-    sTransformed = s # just for demo. In real application, a time consuming but pure calculation
+fib(i::UInt) = i<=1 ? 1 : fib(i-1)+fib(i-2) # I know you can optimize it - compiler can't
+transform(s) = fib(hash(string(s))%48) # just for demo: a pure but expensive function
+
+function f0(::Type{T},s) where {T<:Enum}
+    bits = bitsizeof(T)
+    sTransformed = transform(s) # expensive pure calculation
+    return (T,bits,sTransformed)
+end
+
+@generated function f1(::Type{T},::Val{s}) where {T<:Enum , s}
     bits = Int(Base.invokelatest(bitsizeof,T)) 
+    sTransformed = transform(s) # expensive pure calculation
     return :(($T,$bits,sTransformed))
 end
-export f
-=#
 
-function g(::Type{T},::Val{s}) where {T<:Enum , s}
-    sTransformed = s # just for demo. In real application, a time consuming but pure calculation
-    bits = Int(Base.invokelatest(bitsizeof,T)) 
-    println("T = ",T)
-    ex = :(function Base.reinterpret(::Type{T}, ::Val{s}) where {T <: Enum, s}
-    return (T, $bits, $sTransformed)
-    end)
-    #tModule = getfield(Main,:Main)
-    #println(dump(tModule)
-    #Core.eval(tModule,ex)
-    eval(ex)
-    println("eval done")
-    return ex
+function f2 end
+
+function generate(::Type{T}) where {T<:Enum}
+    bits = bitsizeof(T)
+    for s in instances(T) # generate method per enum instance
+        sTransformed = transform(s) # expensive pure calculation
+        ex = :(function f2(::Type{T}, ::Val{$s}) where {T <: Enum}
+        return (T, $bits, $sTransformed)
+        end)
+        eval(ex)
+    end
+    return nothing
 end
-export g
+export f0,f1,f2,generate
 
 end # module M
 
-module Appusing
+module App
 using Main.M
-@enum MyEnum ::Int8 first = -5 mid = 0 last=5
-g(MyEnum,Val(mid))
+@enum MyEnum ::Int8 e1 = -5 e2 = 0 e3=5
+export MyEnum, e1,e2,e3
+generate(MyEnum)
 end # module app
 
 # in application (another module)
-using Main.M
 using Main.App
+using Main.M
 
-App.f(App.MyEnum,Val(:nix))
+
+f0(App.MyEnum,e2)
+f2(App.MyEnum,Val(e2))
+@time f0(App.MyEnum,e2)
+@time f2(App.MyEnum,Val(e2))
+f1(App.MyEnum,Val(e2))
 
 
 
