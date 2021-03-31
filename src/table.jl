67072a26824e4af0b97a87fad95d64da7a85dab2
Robert Rudolph <rudolph.consulting@gmail.com>
2021-03-31 10:54:30 +0200

    table

--- src/table.jl
+++ src/table.jl
@@ -1,5 +1,10 @@
 # array-like structures using BitStruct-s
 
-mutable struct BSTable{BitStruct{T}} <: AbstractVector{BitStruct{T}} where {T<:NamedTuple}
+mutable struct BSTable{BitStruct{T}} <: AbstractVector{BitStruct{T}}
     base:: Vector{BitStruct{T}}
+    size:: Vector{Int32} # 
+end
+
+struct BSColumn{T,s} <= AbstractVector
+    table :: BSTable{BitStruct{T}}
 end
